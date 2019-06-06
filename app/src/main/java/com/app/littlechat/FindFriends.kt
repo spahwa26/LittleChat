e1f5e48ee4802e4dce93807d81951b786fdbf3be
Sagar <sagar.seraphic@gmail.com>
2019-06-07 01:02:43 +0530

    orderbychild

--- app/src/main/java/com/app/littlechat/FindFriends.kt
+++ app/src/main/java/com/app/littlechat/FindFriends.kt
@@ -1,9 +1,10 @@
 package com.app.littlechat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import android.util.Log
+import com.google.firebase.database.*
 
 class FindFriends : AppCompatActivity() {
 
@@ -17,6 +18,31 @@ class FindFriends : AppCompatActivity() {
     }
 
     private fun init() {
-        database = FirebaseDatabase.getInstance().reference
+        database = FirebaseDatabase.getInstance().getReference("users")
+
+        var quaryData = database.orderByChild("name").startAt("Sh").endAt("Sh" + "\uf8ff")
+
+
+        quaryData .addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                CommonUtilities.hideProgressWheel()
+                if (dataSnapshot.getValue() != null) {
+                    try {
+
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                } else {
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("", "onCancelled: ")
+                CommonUtilities.hideProgressWheel()
+            }
+        })
+
+        Log.e("data", quaryData.toString())
     }
 }
